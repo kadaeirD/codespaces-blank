350e6554ade3f9ff8be44d87dec4e39bad1e02bf
kadaeirD <kdebleye318@boyslatin.net>
2025-09-17 13:37:15 +0000

    classwork

---
+++ unit 1/oprartors.py
@@ -0,0 +1,30 @@
+#operators #print() is a funnction where anything inside the round brackets 
+# will be printed out in the terminal 
+
+#operatirs are construct (build-in sytem) that 
+#data types more actions and power
+
+#arithemtic operator family 
+#arithemtic are simply math operators 
+
+print(2+ 3)  
+# we use mathematical and division eigns to do operations 
+# multiplication and division signs to do oprations 
+print(20 -100) 
+print(40 / 2)
+print(30.9 * 10) 
+
+print("kd") 
+#assignment operatoor family 
+# these operators asign values to varibles 
+#(otherwise known as containers
+# key - value pairs 
+
+schoolname = boys latin high"
+age = 16
+# value  = container for data
+
+
+(age += 1.5) 
+age -=3
+print(age) 
---
+++ unit 2/functions.py
@@ -0,0 +1,101 @@
+# functions a- function is a set of code instructions
+# FOR A COMPUTER TO FOLLOW
+
+# A FUNCTION COMES IN 2 PHASES 
+
+# PHASE 1; FUNCTION DENFINITON
+
+def EXAMPLE():
+    print (" GOODMORNING")       
+    print ("GOOD DAY")      
+
+
+# PHASE 2 FUNCTION CALL/ INVOCATION 
+# WHEN WE WRITE ITO USE IT
+# PRINT()
+EXAMPLE()
+input()
+EXAMPLE2()
+
+
+
+def MATH():
+    A = input( 'ENTER A NUMBER :')
+    B = 10
+    print( A + B)
+MATH()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# function - is a set of code instructions labled under
+# a custom name that the computer will run 
+
+# functiom syntax (rules how its written)
+# functions have 2 phases: function definition and
+# function call
+# phase 1: function definition 
+# we are describing the instuction for our custom code
+# we are addinf logic to the customers vocbulary.
+# this code does not run- it only give the computer the meaning 
+# not action
+def example():
+    print ('goodmoening') # 1 insturction : print good morning 
+ # phase 2: function call
+ # once we  have the definition, we can now the instuctions
+ # by writing the function name
+example    
+
+# we indent to inform the computer that we are about to write
+# code instuctions. if we dont, we will get an error
+
+def example2():
+    print('good day')
+    a = input('enter a number:')
+    print(a)
+
+
+    def math():
+    a = input("please enter an number")
+# create a function that calculates 2 unique user inputted number 
+
+    def calculate():
+     a = input ('please enter a number:')
+     b = input ('please enter another number:')
+print(a + b )
+print(int(a) + int(b)) 
+calculate()
+print(int(a) + int(b)) 
+print(int(a) + int(b)) 
+print(int(a) + int(b)) 
+print(int(a) + int(b)) 
+print(int(a) + int(b)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
