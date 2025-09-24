6c421289861b76b7aaaa7e4e1ea4f7cbf97a8cbd
kadaeirD <kdebleye318@boyslatin.net>
2025-09-24 13:36:45 +0000

    submit

---
+++ conditions.py
@@ -0,0 +1,38 @@
+# conditional statements = code that has a set of  diffrent outcomes
+# outcomes based on the data that is give.
+
+# condtional syntax = if / else
+# if = the condition we are looking to satisfy
+# else = the default / exit. the thing that happens
+# when our condition is not satisfiied.
+
+
+weather = input("Whats the weather like tdy?")
+if weather == 'sunny':
+   print("its gonna be nice out. wear shades.")
+else:
+    print("have a good day.-")
+else:(weather == her 'have a good day')
+
+# creates a conditional block of code that acts as a password 
+# cheker. the user should be able to input a 
+# password. if its is correct, they should get a massage 
+ # saying "welcome, you are logged in". otherwise, they
+ # should get a message saying "try again".
+
+
+
+
+down = input('what down is it ')
+yards = input('how many yards do u need to get another first down?')
+
+ if down == 3 and yards == 2
+     print("run the ball!")
+elif down == 2 yards < 0 
+
+print (you can begin your drive permit t the ages 16)
+
+def permitcheck(age) 
+    age >=16:
+    print(congrats, u can begin learning to drive)''
+    
