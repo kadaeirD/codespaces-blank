55b3caaa88e7959a111a0ce2b04b0f9b5b68853b
kadaeirD <kdebleye318@boyslatin.net>
2025-11-05 14:29:39 +0000

    submit

--- whileactivtyloops.py
+++ whileactivtyloops.py
@@ -21,3 +21,66 @@ def (continuingnumbers)
 print(15 + 13)
 while: (15 < 13)
     print(15): < 13  
+
+
+
+
+
+
+
+
+
+
+# create a function
+# take in user input
+# keep adding numbers to list with a loop
+# if the word entered is quit stop the loop
+# print the list numbers
+
+def numberloop():
+    numberinput = input("please enter a number: ")
+while numberInput != 'quit':  
+   numberlist = []
+   numberlist.append(numberinput)
+   print(numberlist)
+   numberInput = input("please enter a number: ")
+   print('code is working - able to enter numbers')
+else:
+    print('loop has ended. ')
+    print('code is working- able yo quit')
+
+
+numberloop()
+
+# print('code is working things are good)
+
+
+
+
+
+
+
+
+# Acrivity 1
+# create a function that will printout a list of numbers based on
+# what the user inputs. your function should take in a user input
+# add the input to a list and then print it out in the terminal
+#
+#
+
+
+def numberListloop():
+    numberlist = []
+    usernumber = input("type in a number: ")
+    while usernumber != 'quit' :
+        newNumber = int(usernumber)
+        numberlist.append(newNumber)
+        print(numberlist)
+    else:
+        print("loop has stopped. ")
+
+
+
+    def addnumber():
+        numberlist = []
+        
