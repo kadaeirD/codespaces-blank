73aaede28e8daf2de59381518a2426233c97df66
kadaeirD <kdebleye318@boyslatin.net>
2025-11-05 14:33:07 +0000

    classwork

---
+++ unit 2/functions.py
@@ -0,0 +1,32 @@
+#How to create a function with data
+
+#step 1. Function ddefinition
+def bnbRefund(accountNumber, refundamount): # paramters are placeholders
+   print('account number:  '  + str(accountnumber))
+   print("refund amount: $ " + str(refundamopunt))
+
+# step 2. function call (invocation)
+bnbrefund(19292929, 3000) # arguements are real data 
+
+name="kadaeir debleye"
+birthday= "october 30 2007 "
+
+
+def birthdate(name, birthdate):
+       print('my name is '+name + ' and my birthdate is ' + birthdate)
+
+birthdate('Kadaeir ','october 30  ')
+
+# create a function that will convert dollars into pennies
+# your function should take the dollar amount is as a paranter
+# and calculate how many pennie are in their dollar amount
+# your function should print out the following message:
+
+# my {13} dollar(s) is equal to {1300} pennies
+
+
+def dollarConverter(dollar):
+     pennies = dollar * 100
+     print('my'+ str(dollar) + 'dollar(s) is equalt to' + str(pennies) + ' pennies.')
+
+dollarconverter(5) 
